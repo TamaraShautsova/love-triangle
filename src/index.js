d098dc169499728b78cd9138fdb2f13e1683cc4f
tamara shautsova <t.shautsova@gmail.com>
2018-09-23 11:08:36 +0300

    100 test passed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var numberOfLoveTriangles = 0;
+  for (i = 0; i < preferences.length; i++) {
+    if(preferences[i] - 1 == i ) {
+      continue;
+    }
+    if (preferences[preferences[preferences[i]-1]-1]-1 == i) {
+      numberOfLoveTriangles++;
+      preferences[i] = null; //first
+      preferences[preferences[i] - 1] = null; //second
+      preferences[preferences[preferences[i]-1]-1] = null; //third
+    }
+  }
+  return numberOfLoveTriangles;
 };
